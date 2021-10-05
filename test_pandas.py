74e9a5a1cef0f2e96c3fe8ff7a0434a36d933aa4
anurag-gudipati <59970111+anurag-gudipati@users.noreply.github.com>
2021-10-05 12:20:12 -0500

    Update test_pandas.py

--- test_pandas.py
+++ test_pandas.py
@@ -35,42 +35,11 @@ class soccer():
 
 
 
-#filepath = 'C:\Users\Gathi\Documents\test\soccer.csv'
-filepath = 'C:/Users/Gathi/Documents/test/soccer.csv'
+
+filepath = 'soccer.csv'
 df = pd.read_csv(filepath)
 df1 = soccer(filepath)
 print (df1.high_win_perc())
 print (df1.most_draws())
 print (df1.stats_display())
 
-
-
-
-
-
-
-# class soccer():
-#
-#     def __init__(self,filepath):
-#         self.filepath=filepath
-#
-#
-# df = pd.read_csv(r'C:\Users\Anurag\Desktop\soccer.csv')
-#
-# df['win_perc'] = (df['Wins']/df['Games'])*100
-#
-# df = df.sort_values(by=['win_perc'],ascending=False).reset_index(drop=True)
-#
-# df_top_10 = df[:10].copy(deep=True)
-#
-# list_top10_teams = df_top_10['Team'].tolist()
-#
-# df_most_draws = df.sort_values(by=['Draws'],ascending=False).copy(deep=True).reset_index(drop=True)
-#
-# df_most_draws_team = df_most_draws[:1].drop(columns=['win_perc'])
-#
-# print("list_top10_teams", list_top10_teams)
-# print("Team Stats having most draws \n", df_most_draws_team)
-# df1['goal_diff']= df1['Goals'] - df1['Goals Allowed']
-# df1['goal_diff_abs']=df1['goal_diff'].abs()
-# df1.loc[df1['goal_diff_abs'].idxmin()]
